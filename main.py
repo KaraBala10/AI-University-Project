b867739800e1d2bee132f496768946d34a6f39a3
KaraBala10 <mohammad.karabala@gmail.com>
2023-12-09 02:22:59 +0300

    fix some issues and remove XO game

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import random
 import json
 import os
 from difflib import get_close_matches
-from game import tictactoe
 
 BLUE_COLOR = "\033[96m"
 YELLOW_COLOR = "\033[93m"
@@ -23,8 +22,8 @@ def save_knowledge_base(file_path: str, data: dict):
         json.dump(data, file, indent=2)
 
 def find_best_match(user_question: str, questions: list[str]) -> str | None:
-    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
-    return matches[0] if matches else None
+    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.5)
+    return random.choice(matches) if matches else None
 
 def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
     matching_questions = [
@@ -39,9 +38,11 @@ def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
     return None 
 
 def learn_new_word(knowledge_base: dict, user_input: str):
-    existing_question = next(
-        (q for q in knowledge_base["questions"] if q["question"] == user_input), None
-    )
+    existing_question = None
+    for q in knowledge_base["questions"]:
+        if q["question"] == user_input:
+            existing_question = q
+            break
     new_answers: list = (
         input(f'{YELLOW_COLOR}Type the answers separated by commas "," or "skip" to skip: {RESET_COLOR}')
         .strip()
@@ -52,26 +53,26 @@ def learn_new_word(knowledge_base: dict, user_input: str):
         return
     new_answers = [a.strip() for a in new_answers]
     if existing_question:
-        print(
-            f"{BLUE_COLOR }Bot:{RESET_COLOR} I know this question already, but I have learned new answers."
-        )
         existing_answers_set = set(existing_question["answer"])
         existing_question["answer"].extend(
             [a for a in new_answers if a not in existing_answers_set]
         )
-    else:
         print(
-            f'{BLUE_COLOR}Bot:{RESET_COLOR} Thank you! I learned a new question and response.'
+            f"{BLUE_COLOR }Bot:{RESET_COLOR} I know this question already, but I have learned new answers."
         )
+    else:
         knowledge_base["questions"].append(
             {"question": user_input, "answer": new_answers}
+        )
+        print(
+            f'{BLUE_COLOR}Bot:{RESET_COLOR} Thank you! I learned a new question and response.'
         )
     save_knowledge_base("knowledge_base.json", knowledge_base)
 
 def chat_bot():
     knowledge_base: dict = load_knowledge_base("knowledge_base.json")
     print(
-        f'{BLUE_COLOR}Bot:{RESET_COLOR} Hello, I\'m an AI chat. If you want to play with me, send \'play\'. If you want to teach me, send \'teach\' at any time.'
+        f'{BLUE_COLOR}Bot:{RESET_COLOR} Hello, I\'m an AI chat. If you want to teach me, send \'teach\' at any time.'
     )
     while True:
         user_input: str = input(f"{GREEN_COLOR}You: {RESET_COLOR}").lower().strip()
@@ -87,8 +88,6 @@ def chat_bot():
             print(f"{BLUE_COLOR}Bot:{RESET_COLOR} Okay, let's learn. Tell me the question!")
             user_input: str = input(f"{GREEN_COLOR}You: {RESET_COLOR}").lower().strip()
             learn_new_word(knowledge_base, user_input)
-        elif user_input == "play":
-            tictactoe()
         elif not best_match:
             print(f"{BLUE_COLOR}Bot:{RESET_COLOR} I don't know the answer. Please teach me :)")
             learn_new_word(knowledge_base, user_input)
